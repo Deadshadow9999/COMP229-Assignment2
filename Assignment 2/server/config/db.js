459fe30f13ca220c6a70be342629d417194d61da
Henry Pan <legendkiller999@hotmail.com>
2021-10-22 16:29:35 -0400

    Update db.js

--- Assignment 2/server/config/db.js
+++ Assignment 2/server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/user_collection"
+    //"URI": "mongodb://localhost/user_collection"
+    "URI": "mongodb+srv://henrypan001:78ul31w5HDCciN63@mongodbserver.vpzqr.mongodb.net/user_collection?retryWrites=true&w=majority"
 }
